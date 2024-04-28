5a6beccfa4e33e370d37c85a9bbdb3cfb1162eae
Brijesh Singh <bsingh6636@gmail.com>
2024-04-28 18:00:41 +0530

    fixing dev

--- src/component/Body.js
+++ src/component/Body.js
@@ -26,27 +26,28 @@ export const Body = () => {
     const data = await fetch(`${RestroApi}`);
     const jsone = await data.json();
     // console.log(jsone);
-    setrestrolist(
+   
+      const Restrolist =
       jsone.data.cards[1]?.card?.card?.gridElements?.infoWithStyle
         ?.restaurants ||
         jsone.data.cards[2]?.card?.card?.gridElements?.infoWithStyle
           ?.restaurants ||
         jsone.data.cards[3].card.card.gridElements.infoWithStyle.restaurants
-    );
-    setreslists(restrolist);
+        setrestrolist(Restrolist)
+    setreslists(Restrolist);
   } catch(error) {
     console.log('Error fetching from api using api with proxy now', error)
     const proxyData = await fetch(`${proxyRestroApi}`);
     const proxyJson = await proxyData.json();
     // console.log(proxyJson);
-    setrestrolist(
-      proxyJson.data.cards[1]?.card?.card?.gridElements?.infoWithStyle
-        ?.restaurants ||
-        proxyJson.data.cards[2]?.card?.card?.gridElements?.infoWithStyle
-          ?.restaurants ||
-        proxyJson.data.cards[3].card.card.gridElements.infoWithStyle.restaurants
-    );
-    setreslists(restrolist);
+    const proxyRestrolist = proxyJson.data.cards[1]?.card?.card?.gridElements?.infoWithStyle
+  ?.restaurants ||
+  proxyJson.data.cards[2]?.card?.card?.gridElements?.infoWithStyle
+    ?.restaurants ||
+  proxyJson.data.cards[3].card.card.gridElements.infoWithStyle.restaurants;
+
+setrestrolist(proxyRestrolist);
+setreslists(proxyRestrolist);
   }
  }
   const searchrestro = () => {
--- src/component/WhatsOnMind.js
+++ src/component/WhatsOnMind.js
@@ -4,27 +4,25 @@ import { restrodata } from '../MockData/RestroApiData';
 
 export const WhatsOnMind = () => {
     const whatonmind = restrodata.data.cards[0].card.card.imageGridCards.info;
-    const [scrollPosition, setScrollPosition] = useState(0);
     const scrollContainerRef = useRef(null);
 
     const scrollLeft = () => {
         const container = scrollContainerRef.current;
         if (container) {
-            setScrollPosition(scrollPosition - 244); // Assuming each image is 144px wide
+            container.scrollLeft += 244; // Scroll left by 244px
         }
     };
 
     const scrollRight = () => {
         const container = scrollContainerRef.current;
         if (container) {
-            setScrollPosition(scrollPosition + 244); // Assuming each image is 144px wide
+            container.scrollLeft -= 244; // Scroll right by 244px
         }
     };
 
     return (
         <>
             <div className='flex justify-between p-1'>
-            {/* <div> */}
                 <label className="pl-4 pb-1 pt-[16px] font-extrabold text-lg">What's on your mind?</label>
                 <div className="flex items-center">
                     <span className='p-3 mx-1 bg-slate-300 rounded-3xl cursor-pointer' onClick={scrollRight}>&#8592;</span>
@@ -32,13 +30,11 @@ export const WhatsOnMind = () => {
                 </div>
             </div>
 
-            <div ref={scrollContainerRef} className="flex overflow-x-auto gap-x-4 p-2" style={{ scrollbarWidth: 'none', transform: `translateX(${scrollPosition}px)` }}>
-              
-             
+            <div ref={scrollContainerRef} className="flex overflow-x-auto gap-x-4 p-2" style={{ scrollbarWidth: 'none' }}>
                 {
                     whatonmind.map((data, index) => {
                         return (
-                            <img  alt="what's on mind" key={index} className='w-[144px] h-[180px]' src={`${imagelink}${data.imageId}`}></img>
+                            <img alt="what's on mind" key={index} className='w-[144px] h-[180px]' src={`${imagelink}${data.imageId}`}></img>
                         )
                     })
                 }
