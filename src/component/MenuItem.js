a3556084b46fe3013542bc123b92225604f2da6f
Brijesh Singh <bsingh6636@gmail.com>
2024-04-30 14:27:00 +0530

    pushing

--- src/component/MenuItem.js
+++ src/component/MenuItem.js
@@ -5,23 +5,22 @@ import { imagelink } from "./Const";
 export const MenuItem = ({ items }) => {
   const dispatch = useDispatch();
   console.log(items);
-  // Original handlebutton function
-  // const handlebutton = () => {
-  //   dispatch(addItem("pizza"))
-  // }
 
-  // Adjusted handlebutton function to accept an item as an argument
   const handlebutton = (item) => {
-    // Assuming `item` is the specific item you want to add to the cart
     dispatch(addItem(item));
   };
 
   return (
     <div className="border-b border-slate-950  flex justify-between my-2 mx-2 py-2 px-2">
       <div className=" ">
-        <span className="p-5 font-black">{items.card?.info?.name}</span><br></br>
+        <span className="p-5 font-black">{items.card?.info?.name}</span>
+        <br></br>
 
-        <span className="p-5 font-black">₹{items.card?.info?.price / 100}</span>
+        <span className="p-5 font-black">
+          ₹
+          {items.card?.info?.price / 100 ||
+            items.card?.info?.defaultPrice / 100}
+        </span>
       </div>
 
       <div className="flex float-end w-[200px] h-[200px] mr-[-40px]">
@@ -32,7 +31,7 @@ export const MenuItem = ({ items }) => {
         />
         <button
           type="button"
-          className="ml-2 absolute bg-slate-500 text-gray-100 my-1 mx-1 py-2 px-2"
+          className="ml-2 absolute bg-black text-gray-100 my-1 mx-1 py-2 px-2 rounded-lg"
           onClick={() => handlebutton(items)}
         >
           Add +
