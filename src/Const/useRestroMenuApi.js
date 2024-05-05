9f93c97d0d1ad3d5c79cc0f5af48b3991316e83d
Brijesh Singh <bsingh6636@gmail.com>
2024-05-05 18:10:47 +0530

    api changes

--- src/Const/useRestroMenuApi.js
+++ src/Const/useRestroMenuApi.js
@@ -1,29 +1,49 @@
 import { useEffect, useState } from "react";
-import { MenuApi, proxyMenuApi } from "../component/Const";
+import { MenuApi, SpringbeeMenuApi, proxyMenuApi } from "../component/Const";
 export const useRestroMenuApi = (resId) => {
   const [apidata, setapidata] = useState("");
   useEffect(() => {
     const fetchData = async () => {
-      try{
-        console.log(`${MenuApi}${resId}`)
-        console.log(`${proxyMenuApi}${resId}`)
-      const jsone = await fetch(`${proxyMenuApi}${resId}`);
-      const data = await jsone.json();
-      setapidata(data);
-    
-      }
-      catch(error){
-        console.log(`${MenuApi}${resId}`)
-        console.log(`${proxyMenuApi}${resId}`)
-        console.log('failed to fetch menu api using proxyapi now',error);
-        const jsone = await fetch(`${MenuApi}${resId}`);
+      try {
+        // Attempt to fetch data using the proxy
+        const jsone = await fetch(`${proxyMenuApi}${resId}`);
         const data = await jsone.json();
         setapidata(data);
-        console.log(`${proxyMenuApi}${resId}`)
+      } catch (error) {
+        // If the first fetch fails, log the error and attempt to fetch data directly
+        console.log('Failed to fetch menu API using proxy API now');
+        try {
+          const jsone = await fetch(`${MenuApi}${resId}`);
+          const data = await jsone.json();
+          setapidata(data);
+        } catch (error) {
+          // If both fetch attempts fail, use scrapingbee
+          console.log('Failed to fetch menu API directly. Trying scrapingbee...');
+          console.log(`${MenuApi}${resId}`)
+          const url = (`${SpringbeeMenuApi}${resId}`);
+          const options = {
+            method: 'GET',
+            headers: {
+              'X-RapidAPI-Key': '556be648a7mshb9939a516bcc2eap107878jsne68c8cc812bf',
+              'X-RapidAPI-Host': 'scrapingbee.p.rapidapi.com'
+            }
+          };
+
+          try {
+            const response = await fetch(url, options);
+            const result = await response.text();
+            const jsonData = JSON.parse(result); // Parse the JSON data
+            setapidata(jsonData); // Set the parsed JSON data to apidata
+            console.log(jsonData); // Optionally log the parsed data
+          }catch (error) {
+            console.error(error);
+          }
+        }
       }
     };
     fetchData();
   }, [resId]);
+
 
   return apidata;
 };
