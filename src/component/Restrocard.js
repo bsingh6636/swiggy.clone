b9e8615f2beeda8e794b4d6e63161b4629010d3c
Brijesh Singh <bsingh6636@gmail.com>
2024-05-05 23:18:26 +0530

    card changes

--- src/component/Restrocard.js
+++ src/component/Restrocard.js
@@ -12,26 +12,24 @@ export const Restrocard = ({ restro }) => {
     },
   } = restro;
   return (
-    <div className="restro-card p-4 w-[280px] overflow-hidden">
+    <div className="restro-card p-4 w-[280px] overflow-hidden shadow-lg rounded-xl transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-125">
       <img
         className="h-[220px] w-[300px] rounded-xl m-[-30px] "  alt={name}
         src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${cloudinaryImageId}` }
       ></img>
 
-      <div className="RestroDetails mt-8 ">
-        <h3
-          className="font-bold text-pretty p-1"
-         
-        >
-          {name}
-        </h3>
-        <h4  className=" font-bold">
-          ⭐ {avgRating}  •  {deliveryTime} min
-        </h4>
-        <h5 className="font-medium pl-1.5" style={{  fontSize: "small" }}>
-          {cuisines.join(", ")}
-        </h5>
-      </div>
+<div className="RestroDetails mt-8 space-y-2">
+  <h3 className="font-bold text-lg  p-1 bg-gray-200 rounded-md">
+    {name}
+  </h3>
+  <h4 className="font-bold text-sm text-gray-700">
+    ⭐ {avgRating}  •  {deliveryTime} min
+  </h4>
+  <h5 className="font-medium text-xs text-gray-500 pl-1.5">
+    {cuisines.join(", ")}
+  </h5>
+</div>
+
     </div>
   );
 };
