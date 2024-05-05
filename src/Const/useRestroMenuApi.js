5eb4df8d1a574fcffaf45f00373cd481a956e715
Brijesh Singh <bsingh6636@gmail.com>
2024-05-05 12:01:52 +0530

    ch

--- src/Const/useRestroMenuApi.js
+++ src/Const/useRestroMenuApi.js
@@ -5,15 +5,21 @@ export const useRestroMenuApi = (resId) => {
   useEffect(() => {
     const fetchData = async () => {
       try{
+        console.log(`${MenuApi}${resId}`)
+        console.log(`${proxyMenuApi}${resId}`)
       const jsone = await fetch(`${MenuApi}${resId}`);
       const data = await jsone.json();
       setapidata(data);
+    
       }
       catch(error){
+        console.log(`${MenuApi}${resId}`)
+        console.log(`${proxyMenuApi}${resId}`)
         console.log('failed to fetch menu api using proxyapi now',error);
         const jsone = await fetch(`${proxyMenuApi}${resId}`);
         const data = await jsone.json();
         setapidata(data);
+        console.log(`${proxyMenuApi}${resId}`)
       }
     };
     fetchData();
