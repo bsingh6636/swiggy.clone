3a9c3eee665a8c5faf05daef4e4c1eac9390190c
Brijesh Singh <bsingh6636@gmail.com>
2024-11-03 21:34:57 +0530

    changes

--- src/Const/fetchproxy.js
+++ src/Const/fetchproxy.js
@@ -1,6 +1,6 @@
  // It's recommended to use environment variables for 
  // sensitive information like API keys.
- const API_KEY = 'f3606db5a7ed0f51';
+ const API_KEY = '11006d1b531e3c96';
  const EDUCORS_URL ='https://educorssolver.host/api/getData';
 
  // Function to get data from the API
--- src/Const/restroApi.js
+++ src/Const/restroApi.js
@@ -2,7 +2,7 @@
 
 export const fetchdata = async () => {
   try {
-    const API_KEY = 'f3606db5a7ed0f51';
+    const API_KEY = '11006d1b531e3c96';
     const EDUCORS_URL ='https://educorssolver.host/api/getData';
     const TARGET_URL ='https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.1017167&lng=77.634826600000011';
 
